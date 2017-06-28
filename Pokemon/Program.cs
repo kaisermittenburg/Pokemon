831690a8b2469577ff7e29d99ecc3b190cc4349e
kaisermittenburg <kaisermittenburg@gmail.com>
2017-06-27 19:00:12 -0500

    Admin

---
+++ Pokemon/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pokemon
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
