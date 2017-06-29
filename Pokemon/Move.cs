35baa4b1c3ae380bb2150dd9fb3f762c12605ad0
kaisermittenburg <kaisermittenburg@gmail.com>
2017-06-28 22:22:00 -0500

    Admin MoveIndex

--- Pokemon/Move.cs
+++ Pokemon/Move.cs
@@ -3,14 +3,16 @@ namespace Pokemon
 {
     public class Move
     {
-        public Move(string type)
+        public Move()
         {
-            Initialize(type);
+            
         }
         public string name;
+        public string group;
         public string moveType;
         public int hitChance;
         public int maxDamage;
+        public bool special;
 
         private void Initialize(string type)
         {
--- Pokemon/MoveIndex.cs
+++ Pokemon/MoveIndex.cs
@@ -8,10 +8,27 @@ namespace Pokemon
     {
         public MoveIndex()
         {
-
+            Initialize();
         }
 
         List<Move> Moves = new List<Move>();
+        private void Initialize()
+        {
+            Moves.Add(new Move() {name = "Swipe", moveType = "Physical", group = "all", special = false, hitChance = 80, maxDamage = 18});
+            Moves.Add(new Move() { name = "Pounce", moveType = "Physical", group = "all", special = false, hitChance = 55, maxDamage = 29 });
+            Moves.Add(new Move() { name = "FireBall", moveType = "Element", group = "Fire", special = false, hitChance = 70, maxDamage = 20 });
+            Moves.Add(new Move() { name = "Napalm", moveType = "Element", group = "Fire", special = true, hitChance = 35, maxDamage = 35 });
+            Moves.Add(new Move() { name = "Sprinkler", moveType = "Element", group = "Water", special = true, hitChance = 90, maxDamage = 15 });
+            Moves.Add(new Move() { name = "Hail", moveType = "Element", group = "Water", special = true, hitChance = 100, maxDamage = 15 });
+            Moves.Add(new Move() {name = "Defend", moveType = "Defense", group = "all", special = true, hitChance = 100, maxDamage = 0});
+        }
+    }
+    public class SpecialMoveIndex
+    {
+        public SpecialMoveIndex()
+        {
+            
+        }
     }
 
 
