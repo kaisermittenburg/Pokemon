6d29f72677c08ae76ef0014e591fcbff85dcb9f6
kaisermittenburg <kaiser.mittenburg@mediware.com>
2017-07-06 23:29:00 -0500

    Admin

--- Pokemon/Colosseum.cs
+++ Pokemon/Colosseum.cs
@@ -70,6 +70,7 @@ namespace Pokemon
             WriteLine("Player, please build your Pokemon!");
             WriteLine("Enter its name");
             pokemon.Name = ReadLine();
+            Console.Clear();
             WriteLine("Choose its type (number) \n1) Grass\n2) Fire\n3) Water\n4) Rock\n5) Mystic");
             int.TryParse(ReadLine(), out int type);
             switch(type)
@@ -90,6 +91,7 @@ namespace Pokemon
                     pokemon.Type = "Mystic";
                     break;
             }
+            Console.Clear();
             WriteLine("Divide 100 points between Attack and Defense");
             WriteLine("Enter attack points (0 - 100)");
             int.TryParse(ReadLine(),out int attack);
@@ -98,6 +100,7 @@ namespace Pokemon
                 Console.WriteLine("Your entry is invalid, please enter a number between 0 and 100.");
                 int.TryParse(ReadLine(), out attack);
             }
+            Console.Clear();
             WriteLine("Enter defense points (0 - " + (100 - attack) + ")");
             int.TryParse(ReadLine(), out int defense);
             while (defense < 0 || defense > 100 - attack)
